3eb9185d4691e54c4382aaa7991e2cbf1ac4eb5d
Jacob-reg <159228288+Jacob-reg@users.noreply.github.com>
2024-12-03 23:12:37 +0800

    Update readScripts.py

--- readScripts.py
+++ readScripts.py
@@ -1,1 +1,22 @@
+def find_university():
+    """
+    Updates a  university document in the university collection.
 
+    Parameters:
+        university: Name of the university. (String)
+        abbreviation: Abbreviation of the university (String).
+    """
+
+    conn = openConnection()
+    db = conn['dbName']			        # Change dbName accordingly
+    collection = db['universities']
+
+    result = collection.find()
+
+    for doc in result:
+      print(doc)
+    print(f"{university}, ({abbreviation}) updated")
+
+    closeConnection(conn)
+
+    return result
