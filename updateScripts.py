713b5a96353900a1e1091c6080e7bbc3c57110f3
Jacob-reg <159228288+Jacob-reg@users.noreply.github.com>
2024-12-03 23:03:49 +0800

    Update updateScripts.py

--- updateScripts.py
+++ updateScripts.py
@@ -1,22 +1,22 @@
-def update_university(university_name, new_name, new_abbreviation):
+def update_university(university, new_name, new_abbreviation):
     """
     Updates a  university document in the university collection.
 
     Parameters:
-        university_name: Name of the university. (String)
+        university: Name of the university. (String)
         abbreviation: Abbreviation of the university (String).
-    Returns:
-        inserted_id: ID of the created university document. (ObjectId)
     """
 
     conn = openConnection()
     db = conn['dbName']			        # Change dbName accordingly
     collection = db['universities']
 
-    result = collection.update_one( 
-{'name': university_name}, { 'name': new_name, 'abbreviation': new_abbreviation }} 
-)
-    print(f"{university_name}, ({abbreviation}) updated")
+    results = collection.update_one(
+    {'university': university},
+    {'$set': {'university': new_name, 'abbreviation': new_abbreviation}}
+    )  
+    
+    print(f"{university}, ({abbreviation}) updated")
 
     closeConnection(conn)
 
