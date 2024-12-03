8750258de051affc78587c2cfe42fbd105fdca72
Jacob-reg <159228288+Jacob-reg@users.noreply.github.com>
2024-12-04 01:38:06 +0800

    Update readScripts.py

--- readScripts.py
+++ readScripts.py
@@ -34,3 +34,47 @@ def viewPlayerProfile(playerID):
       print(doc)
 
     closeConnection(conn)
+
+def viewTeamProfile(playerID):
+    """
+    Access Pattern:
+    View Team Profile: User views team profile
+    """
+
+    conn = openConnection()
+    db = conn['uaap_esports']			        # Change dbName accordingly
+    collection = db['players']
+
+    result = collection.find_one({"_id": teamID})
+    
+    for doc in result:
+      print(doc)
+
+    closeConnection(conn)
+
+
+
+
+def teamTournamentHistory(teamID):
+    """
+    Access Pattern:
+    View Team Tournament Hisoty: User team tournament history
+    """
+    conn = openConnection()
+    db = conn['uaap_esports']			        # Change dbName accordingly
+    collection = db['tournament']
+
+
+    collection.aggregate([
+        {
+            $match: {
+                teams: { $in: [teamID] }
+            }
+        },
+        {
+            $project: {
+                _id: 1
+            }
+        }
+    ])
+    closeConnection(conn)
